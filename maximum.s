f994fa43ba89c6f8c24824f285704d203d051736
Niranjan Mallapadi <niranjan.ashok@gmail.com>
2013-09-08 00:23:57 +0530

    Added comments to the program to understand it better

--- maximum.s
+++ maximum.s
@@ -5,23 +5,23 @@ data_items:
 .section .text
 .globl _start
 _start:
-	movl $0, %edi				#initialize edi to 0
-	movl $data_items+0 , %edx		# we are storing address of first element in edx
-	movl $data_items+56, %ecx		#we are storing the address of last element to ecx
+	movl $0, %edi			# initialize edi to 0
+	movl $data_items+0 , %edx	# we are storing address of first element in edx
+	movl $data_items+56, %ecx	# we are storing the address of last element to ecx
 	movl data_items(,%edi,4), %eax
 	movl %eax, %ebx
 start_loop:
-	cmpl %edx, %ecx				#we are checking if edx has moved to last element
-	je loop_exit
-	addl $4, %edx
-	incl %edi
-	movl data_items(,%edi,4), %eax
-	cmpl %ebx, %eax
-	jle start_loop
+	cmpl %edx, %ecx			# we are checking if edx has moved to last element
+	je loop_exit			# if starting and ending address are same we exit the loop
+	addl $4, %edx			# We want to move by 4 bytes so we increment edx by 4 
+	incl %edi			# We increment %edi by 1 
+	movl data_items(,%edi,4), %eax	# Access the next data item
+	cmpl %ebx, %eax			# compare the current item with existing largest value
+	jle start_loop			# if the current item is less, we repeat
 	
-	movl %eax, %ebx
-	jmp start_loop
+	movl %eax, %ebx			# if current item is highest, we interchange and update ebx with highest value
+	jmp start_loop			# we repeat steps till we reach end of the loop
 
 loop_exit:
-	movl $1, %eax
-	int $0x80
+	movl $1, %eax			# call exit system call
+	int $0x80			# Call Bruce Wayne
