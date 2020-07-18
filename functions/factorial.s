339b6b2b7461052078e76a9c0a759c3b448fc3bf
Niranjan Mallapadi <niranjan@ashoo.in>
2020-07-18 14:16:27 +0530

    Functions: Example program using factorial
    Signed-off-by: Niranjan Mallapadi <niranjan@ashoo.in>

--- functions/factorial.s
+++ functions/factorial.s
@@ -1,29 +1,33 @@
+# PURPOSE: Given a number, this program computes the
+# factorial. For example the factorial of
+# 3 is 3 * 2 * 1 or 6 the factorial of
+# 4 is 4 * 3 * 2 * 1, or 24 and so on.
+	
 .section .text
 .globl _start
 _start:
-	pushl $4
-	call ohhmyfac
-	movl $1, %eax
-	int $0x80
+	pushl $4 		# pass 4 to stack (function)
+	call factorial		# call factorial function
+	addl $4, %esp		# bring the esp back to the original value
+	movl %eax, %ebx		# save the result in %ebx
+	movl $1, %eax		# call exit function
+	int $0x80		# exit
 
-.type ohhmyfac, @function
-ohhmyfac:
-	pushl %ebp
-	movl %esp, %ebp
-	subl $4, %esp
-	movl 8(%ebp), %ecx
-	movl %ecx, -4(%ebp)
-start_loop:
-	movl -4(%ebp), %eax
-	decl %ecx
-	imull %ecx, %eax
-	movl %eax, -4(%ebp)
-	cmpl $1, %ecx
-	je end_loop
-	jmp start_loop
-
-end_loop:
-	movl -4(%ebp), %ebx
+.type factorial, @function	
+factorial:
+	pushl %ebp		# save old ebp
+	movl %esp, %ebp		# we use base pointer to access stack values
+	movl 8(%ebp), %eax	# get the first argument to stack in to %eax
+	cmpl $1, %eax		# if the number is one this our base case and we  
+	je end_factorial	# simply go to the end of the function
+	decl %eax		# othe wise decrement eax
+	pushl %eax		# we push that value to stack 
+	call factorial		# call factorial 
+	movl 8(%ebp), %ebx	# %eax has return value so we reload that our
+				# parameter in to %ebx
+	imull %ebx, %eax	# multiply the result with last call to factorial
+				# answer is stored in %eax
+end_factorial:
 	movl %ebp, %esp
 	popl %ebp
 	ret
