6ab5af1f1c30f1b3ccea034c1cbcc676452ad6e6
Niranjan M.R <niranjan@ashoo.in>
2024-06-29 19:27:33 -0400

    fix finding maximum of given numbers in riscv32
    Signed-off-by: Niranjan M.R <niranjan@ashoo.in>

--- rv32/math/maximum.s
+++ rv32/math/maximum.s
@@ -1,20 +1,20 @@
 	.section .data
 data_items:
-	.word 3,67,44,23,22 
+	.word 99,3,67,44,23,102,22 
 	.section .text
 	.globl _start
 _start:
-	la x5, data_items
-	addi x7, x0, 0
-	la x2, data_items
-	lw x6, 16(x5)
-	lw x10, (x2)
+	la x5, data_items # load first items address to x5
+	la x2, data_items # store first items address to x2
+	lw x6, 28(x5)  # last item 22 is stored in x6
+        lw x10, (x2)  # x10 stores the current maximum
 start_loop:
-	beq x10, x6, exit
-	addi x2, x2, 4
-	lw x7, 0(x2)
-	blt x7, x10, start_loop
-	lw x10, x4
+	lw x15, (x2)  # move the current value stored in address x2 to x10
+	beq x15, x6, exit   # compare x10 and x6 are equal
+	addi x2, x2, 4  # increment address stored in x2 to 4 
+	lw x4, 0(x2)   #store first value stored in address stored in x2 to x7
+	blt x4, x10, start_loop #compare x4 and x10 x7 < x10 jump to start_loop
+	add x10, x4, 0  # store thevalue in x4 
 	j start_loop
 exit:	
 	li x17, 93
